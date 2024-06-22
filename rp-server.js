74533e944754575c5ff929756f3d4055efc4d5ce
naganithin <naganithinsydney@gmail.com>
2024-06-22 13:29:50 +0530

    corsOptions

--- rp-server.js
+++ rp-server.js
@@ -20,7 +20,13 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Allow these methods
+    allowedHeaders: ['Content-Type', 'Authorization'],  // Allow these headers
+};
+
+app.use(cors(corsOptions));
 
 async function createContact(contactData) {
     try {
